cff53de7d3d5ec75a215845ef8953dab21581193
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2022-02-26 18:15:57 +0100

    Fixed to work with the latest version

--- src/main/java/com/github/geysercustommodeldata/templategeyserextension/custom/MagicStick.java
+++ src/main/java/com/github/geysercustommodeldata/templategeyserextension/custom/MagicStick.java
@@ -1,10 +1,11 @@
 package com.github.geysercustommodeldata.templategeyserextension.custom;
 
 import org.geysermc.geyser.api.custom.items.CustomItemData;
+import org.geysermc.geyser.api.custom.items.registration.CustomModelDataItemType;
 
 public class MagicStick extends CustomItemData {
     public MagicStick() {
-        super(502698, "magic_stick");
+        super(new CustomModelDataItemType(502698), "magic_stick");
 
         this.setIsTool(true);
         this.setTextureSize(32);
